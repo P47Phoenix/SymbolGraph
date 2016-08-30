5d1fa930fb9dea780513269010d214d366502d7e
P47Phoenix <michaelconne@gmail.com>
2016-08-29 21:03:14 -0500

    Added packages

---
+++ MC.Agent.PlayGround/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Application.Threading;
+using JetBrains.DocumentManagers;
+using JetBrains.Platform.RdFramework;
+using JetBrains.ProjectModel.Impl;
+using JetBrains.ProjectModel.Model2.Transaction;
+using JetBrains.Rider.Model;
+using JetBrains.Util;
+
+namespace MC.Agent.PlayGround
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
