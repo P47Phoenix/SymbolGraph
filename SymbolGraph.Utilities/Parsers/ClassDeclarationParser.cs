46eb809d2d296586c50b1513ff1b5768264322c2
Michael Connelly <michaelconne@gmail.com>
2022-02-17 21:18:59 -0600

    Update ClassDeclarationParser.cs

--- SymbolGraph.Utilities/Parsers/ClassDeclarationParser.cs
+++ SymbolGraph.Utilities/Parsers/ClassDeclarationParser.cs
@@ -4,8 +4,48 @@ namespace SymbolGraph.Utilities;
 
 public class ClassDeclarationParser : IParser<ClassDeclarationSyntax, DocumentClassDeclaration>
 {
-    public Task<DocumentClassDeclaration> ParseAsync(ClassDeclarationSyntax item)
+    private readonly IParser<PropertyDeclarationSyntax, DocumentPropertyDeclaration> _propertyDeclarationParser;
+    private readonly IParser<MethodDeclarationSyntax, DocumentMethodDeclaration> _methodDeclarationParser;
+
+    public ClassDeclarationParser(
+        IParser<PropertyDeclarationSyntax, DocumentPropertyDeclaration> propertyDeclarationParser,
+        IParser<MethodDeclarationSyntax, DocumentMethodDeclaration> methodDeclarationParser)
     {
-        throw new NotImplementedException();
+        _propertyDeclarationParser = propertyDeclarationParser;
+        _methodDeclarationParser = methodDeclarationParser;
+    }
+    
+    public async Task<DocumentClassDeclaration> ParseAsync(ClassDeclarationSyntax item)
+    {
+        var documentClassDeclaration = new DocumentClassDeclaration();
+        
+        var propertyDeclarationSyntaxNodes = item
+            .ChildTokens()
+            .OfType<PropertyDeclarationSyntax>()
+            .ToList();
+        
+        
+
+        foreach (var propertyDeclarationSyntaxNode in propertyDeclarationSyntaxNodes)
+        {
+            var propertyDeclaration = await _propertyDeclarationParser.ParseAsync(propertyDeclarationSyntaxNode);
+            
+            documentClassDeclaration.Properties.Add(propertyDeclaration);
+        }
+        
+        
+        var methodDeclarationSyntaxNodes = item
+            .ChildTokens()
+            .OfType<MethodDeclarationSyntax>()
+            .ToList();
+
+        foreach (var methodDeclarationSyntaxNode in methodDeclarationSyntaxNodes)
+        {
+            var documentMethodDeclaration = await _methodDeclarationParser.ParseAsync(methodDeclarationSyntaxNode);
+            
+            documentClassDeclaration.Methods.Add(documentMethodDeclaration);
+        }
+        
+        return documentClassDeclaration;
     }
 }
